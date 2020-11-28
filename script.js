536860500a2a6f5ba77f79e9459fb91778e12ff3
Doziem <doziem.okemini@aun.edu.ng>
2020-11-28 18:49:47 +0100

    Added Attribution

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 let randomNumber = Math.floor(Math.random() * 9);
 let messages = [
     {
-        text: "The starry vault of heaven is in truth the open book of cosmic projection…"
+        text: "The starry vault of heaven is in truth the open book of cosmic projection…  [—Carl Jung]"
     },
 
     {
-        text: "Astrology is a Language. If you understand this language, The Sky Speaks to You."
+        text: "Astrology is a Language. If you understand this language, The Sky Speaks to You. [—Dane Rudhyar]"
     },
 
     {
-        text: "Your path is illuminated by a road-map of stars. I am here to guide you!"
+        text: "Your path is illuminated by a road-map of stars. I am here to guide you! [—Ambika Devi]"
     },
 
     {
-        text: "I was born during an eclipse. I believe very much in astrology. If you were born on an eclipse it indicates your destiny is chaotic."
+        text: "I was born during an eclipse. I believe very much in astrology. If you were born on an eclipse it indicates your destiny is chaotic. [—Gloria Vanderbilt]"
     },
 
     {
-        text: "The way that I see astrology is as a repository of thought and psychology. A system we’ve created as a culture as way to make things mean things."
+        text: "The way that I see astrology is as a repository of thought and psychology. A system we’ve created as a culture as way to make things mean things. [—Eleanor Catton]"
     },
 
     {
-        text: "There is no better boat than a horoscope to help a man cross over the sea of life."
+        text: "There is no better boat than a horoscope to help a man cross over the sea of life. [—Varaha Mihira]"
     },
 
     {
-        text: "The astronomer has a starry map of the past; the astrologer, of our futures."
+        text: "The astronomer has a starry map of the past; the astrologer, of our futures. [—Terri Guillemets]"
     },
 
     {
-        text: "I will look on the stars and look on thee, and read the page of thy destiny."
+        text: "I will look on the stars and look on thee, and read the page of thy destiny. [—Letitia Elizabeth Landon]"
     },
 
     {
-        text: "Though Astrology is like a deep ocean…anybody can get knowledge through going deeply in water and get some drops of nectar of this divine knowledge."
+        text: "Though Astrology is like a deep ocean…anybody can get knowledge through going deeply in water and get some drops of nectar of this divine knowledge. [—Onkarlal Sharma Prmad]"
     }
 
 ]
