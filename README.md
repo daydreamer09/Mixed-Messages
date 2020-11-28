532835c09e377a00f34d9685f6a5abd91a4bf74a
Doziem <doziem.okemini@aun.edu.ng>
2020-11-28 13:03:28 +0100

    mixed messages first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Message Generator
+ This is a program that prints out random Astrology quotes form 
+ an *object*. It is a simple progrmn testing the use of **Objects**
+## Technologies
+    Javascript
---
+++ script.js
@@ -0,0 +1,41 @@
+let randomNumber = Math.floor(Math.random() * 9);
+let messages = [
+    {
+        text: "The starry vault of heaven is in truth the open book of cosmic projection…"
+    },
+
+    {
+        text: "Astrology is a Language. If you understand this language, The Sky Speaks to You."
+    },
+
+    {
+        text: "Your path is illuminated by a road-map of stars. I am here to guide you!"
+    },
+
+    {
+        text: "I was born during an eclipse. I believe very much in astrology. If you were born on an eclipse it indicates your destiny is chaotic."
+    },
+
+    {
+        text: "The way that I see astrology is as a repository of thought and psychology. A system we’ve created as a culture as way to make things mean things."
+    },
+
+    {
+        text: "There is no better boat than a horoscope to help a man cross over the sea of life."
+    },
+
+    {
+        text: "The astronomer has a starry map of the past; the astrologer, of our futures."
+    },
+
+    {
+        text: "I will look on the stars and look on thee, and read the page of thy destiny."
+    },
+
+    {
+        text: "Though Astrology is like a deep ocean…anybody can get knowledge through going deeply in water and get some drops of nectar of this divine knowledge."
+    }
+
+]
+
+console.log(messages[randomNumber].text);
